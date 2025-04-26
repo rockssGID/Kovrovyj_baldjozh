e6f1e59ed817d070f6303430eb904db92e9a6794
rockssGID <rockssaa@gmail.com>
2025-04-27 02:00:06 +0300

    Удалил папку

--- setting/gulp/tasks/zip.js
+++
@@ -1,7 +0,0 @@
-import zip from 'gulp-zip';
-
-export const zipBuild = () => {
-  return app.gulp.src(app.path.buildFolder)
-    .pipe(zip('build.zip'))
-    .pipe(app.gulp.dest(app.path.zipFolder));
-};
